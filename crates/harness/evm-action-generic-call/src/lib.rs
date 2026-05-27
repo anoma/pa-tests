72365ea897ff6a08abe7826a603be77ddd1a96a0
Tiago Carvalho <sugoiuguu@tfwno.gf>
2026-05-27 15:39:20 +0200

    Add generic call action helper crate

---
+++ crates/harness/evm-action-generic-call/src/lib.rs
@@ -0,0 +1,8 @@
+mod action;
+mod resource;
+
+pub use action::GenericCallActionParts;
+pub use action::build_generic_call_action;
+pub use action::build_generic_call_action_with_overrides;
+pub use resource::GenericCallActionOverrides;
+pub use resource::generic_call_vk;
